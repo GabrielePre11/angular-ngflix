163263a7cfaa52f744e8ff95b31976fc4a50ebf8
Gabriele Prestano <gabryprestanoitaly@gmail.com>
2025-10-30 14:14:30 +0100

    Fixed Container

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Sidebar } from './core/components/sidebar/sidebar';
 import { Header } from './core/components/header/header';
 import { Footer } from './core/components/footer/footer';
 import { CommonModule } from '@angular/common';
+import { Container } from './core/layout/container/container';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, Sidebar, Header, Footer],
+  imports: [CommonModule, RouterOutlet, Sidebar, Header, Footer, Container],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/core/layout/container/container.ts
+++ src/app/core/layout/container/container.ts
@@ -9,4 +9,5 @@ import { Component, input } from '@angular/core';
 })
 export class Container {
   className = input<string>('');
+  isSidebarOpen = input.required<boolean>();
 }
