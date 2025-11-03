dcce3466da70f2254089d56010794b259c8ec0e9
Gabriele Prestano <gabryprestanoitaly@gmail.com>
2025-11-03 18:54:48 +0100

    Added RouterLink on hero-banners

--- src/app/shared/banner/banner.ts
+++ src/app/shared/banner/banner.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './banner.html',
   styleUrl: './banner.css',
 })
